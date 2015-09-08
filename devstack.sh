e01cf518c3ec2e81e9f49ee8deef834d31d92b65
Youhei Tooyama <tooyama@virtualtech.jp>
2015-09-08 15:18:04 +0900

    local.confのリンク貼り処理をきちんとif文で制御

--- devstack.sh
+++ devstack.sh
@@ -3,12 +3,18 @@
 rm -rf devstack
 git clone -b stable/kilo https://git.openstack.org/openstack-dev/devstack
 
-ln -s ~/devstack-memo/local.conf devstack/local.conf
+if [ -h devstack/local.conf ];
+then
+   echo "local.conf is found."
+else
+   ln -s ~/devstack-memo/local.conf devstack/local.conf
+   echo "ln -s is done."
+fi
 
-echo "run the stack.sh? Cancel:(ctrl+c)"
+echo "Run the stack.sh? Cancel:(ctrl+c)"
 read confstack1
 bash devstack/stack.sh
 
-echo "configure br-ex? Cancel:(ctrl+c)"
+echo "Configure br-ex? Cancel:(ctrl+c)"
 read confstack2
 sudo ifconfig br-ex 0.0.0.0 && sudo ovs-vsctl add-port br-ex eth0
